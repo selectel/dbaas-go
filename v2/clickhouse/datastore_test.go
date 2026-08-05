cdd26fd256963ea7c88fe47671b678bc3e62cde4
dbalasov <balasov.denis@yandex.ru>
2026-08-05 21:36:53 +0300

    fix lint errors

--- v2/clickhouse/datastore_test.go
+++ v2/clickhouse/datastore_test.go
@@ -7,10 +7,11 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/selectel/dbaas-go/internal/transport"
 	"github.com/selectel/dbaas-go/v2/common"
 	"github.com/selectel/dbaas-go/v2/internal"
-	"github.com/stretchr/testify/require"
 )
 
 func newDatastoreService(t *testing.T, serverURL string) *DatastoreService {
